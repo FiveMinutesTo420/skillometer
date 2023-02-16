64d535784b22138dfcf126a399bea0e961ad110d
ayaal_fedorov <aiaal8964@mail.ru>
2023-02-16 14:12:14 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      boxShadow:{
+        'custom':"-44 52px 8px 0px rgba(34,60,80,0.2)"
+      }
+    },
+  },
+  plugins: [],
+}
