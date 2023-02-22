2ed3d164214edf35029e527cf19b264a9d9cb5be
ayaal_fedorov <aiaal8964@mail.ru>
2023-02-22 09:25:12 +0900

    change app_password

--- sendmail.php
+++ sendmail.php
@@ -18,21 +18,21 @@ $mail->isSMTP();
 $mail->Host = "smtp.mail.ru";
 $mail->SMTPAuth = true;
 $mail->Username = "aiaal8964@mail.ru";
-$mail->Password = "48GX17tGZT5WF6mXntHV";
+$mail->Password = "q0e6KR4HHHmw3jUiB8Rq";
 $mail->SMTPSecure = "tls";
 $mail->Port = 587;
 
 $mail->From = "aiaal8964@mail.ru";
 $mail->FromName = "Федоров Айаал";
 
-
+$mail->CharSet = "UTF-8";
 //Адрес куда будут поступать заявки
-$mail->addAddress("skillometer@mail.ru", "Skillometer");
+$mail->addAddress("aiaal8964@mail.ru", "Skillometer");
 
 
 $mail->isHTML(true);
 //Сообщение
-$mail->Subject = "Заявка: " . $name . " " . $email;
+$mail->Subject = "Заявка: " . $name . ", " . $email . ", " . $number;
 $mail->Body = $content;
 $mail->AltBody = "";
 
